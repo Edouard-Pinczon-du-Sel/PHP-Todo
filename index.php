f99933a909f08960443008d65f8abf8d8c10973c
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2023-06-23 18:36:15 +0200

    update: make html structure and styles

--- index.php
+++ index.php
@@ -5,12 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
     <script defer src="public/js/index.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Open+Sans:wght@700&display=swap" rel="stylesheet">
     <title>Todo</title>
 </head>
 <body>
-    <header>HEADER</header>
-    <div class="content">CONTENT</div>
-    <footer>FOOTER</footer>
+    <div class="container">
+        <header>
+            <div class="logo">Edouard TODO</div>
+        </header>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form">
+                    <div class="todo-list">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <p>2023 / 2024 @ Tous droits réservés</p>
+        </footer>
+    </div>
+    
     
 </body>
 </html>
