012e5163024af04698c7ced718980df73208877f
Edouard-Pinczon-du-Sel <edouardpinczon@gmail.com>
2023-06-23 18:52:12 +0200

    update : php includes structure

---
+++ includes/header.php
@@ -0,0 +1,3 @@
+<header>
+    <div class="logo">Edouard TODO</div>
+</header>
--- index.php
+++ index.php
@@ -1,33 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/style.css">
-    <script defer src="public/js/index.js"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+    <?php require_once 'includes/head.php'?>
     <title>Todo</title>
 </head>
 <body>
     <div class="container">
-        <header>
-            <div class="logo">Edouard TODO</div>
-        </header>
+        <?php require_once 'includes/header.php'?>
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
                 <div class="todo-form">
-                    <div class="todo-list">
-
-                    </div>
+                    <div class="todo-list"></div>
                 </div>
             </div>
         </div>
-        <footer>
-            <p>2023 / 2024 @ Tous droits réservés</p>
-        </footer>
+        <?php require_once 'includes/footer.php'?>
     </div>
     
     
